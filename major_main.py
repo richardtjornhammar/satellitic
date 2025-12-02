42587823858137cb43a987b094af008c77ccdc3a
pts-rictjo <richard.tjornhammar@pts.se>
2025-12-02 14:48:06 +0100

    Update lic

--- major_main.py
+++ major_main.py
@@ -1,7 +1,20 @@
-#!/usr/bin/env python3
-"""
-full_research_simulator.py
+lic_ = """
+   Copyright 2025 Richard Tjörnhammar
 
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+desc_ = """
 Full research-grade satellite multibeam coverage simulator with hybrid TLE source:
  - Primary: CelesTrak (gp.php?GROUP=...)
  - Fallback: local TLE file (one TLE block per 3-line entry)
@@ -812,3 +825,4 @@ if __name__ == "__main__":
     print ( tdf )
 
     tdf .describe()
+
