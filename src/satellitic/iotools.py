14cdd3fce631d4069102474c736ea388f4e5bf31
Richard Tjörnhammar <richard.tjornhammar@pts.se>
2026-02-26 14:20:54 +0100

    Update iotools.py

--- src/satellitic/iotools.py
+++ src/satellitic/iotools.py
@@ -1,5 +1,5 @@
 lic_ = """
-   Copyright 2025 Richard Tjörnhammar
+   Copyright 2026 Richard Tjörnhammar
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
